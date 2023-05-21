9bdb0460c93958acf19196737e326c5e74e49147
unknown <shrikantdamahe143@hmail.com>
2023-05-21 18:17:13 +0530

    Commands

---
+++ src/Componend/Assignment_day1/list.js
@@ -0,0 +1,12 @@
+import React from 'react'
+ function List() {
+ return <ol>
+    <li>Aman</li> 
+    <li>Manthan</li> 
+    <li>Akash</li> 
+    <li>Piyush</li> 
+    <li>Rahat</li>
+ </ol>
+ 
+}
+export default List;
---
+++ src/Componend/Assignment_day1/title.js
@@ -0,0 +1,5 @@
+import React from 'react'
+ function Title() {
+    return <h1>people Name</h1>
+ }
+  export default Title;
